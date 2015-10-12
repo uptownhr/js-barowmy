aa69ac210d15e38951e901a7409db67826ac295b
James Lee <uptownhr@gmail.com>
2015-10-12 16:26:52 -0700

    added tolowercase

--- models/Vendor.js
+++ models/Vendor.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose')
 const imageSchema = require('./imageSchema')
 
 const locationSchema = new mongoose.Schema({
-  text: {type: String},
+  text: {type: String, lowercase: true},
   coordinates: {type: String},
   number: {type: Number},
   unit: {type: String},
-  street: {type: String},
+  street: {type: String, lowercase: true},
   regions: {type: [String]},
-  city: {type: String},
-  state: {type: String},
+  city: {type: String, lowercase: true},
+  state: {type: String, lowercase: true},
   postalcode: {type: String},
   created_at: {type: Date, default: Date.now }
 })
@@ -18,8 +18,8 @@ const vendorSchema = new mongoose.Schema({
   name: {type: String, required: true, unique: true},
   description: {type: String},
   phone: {type: String},
-  email: {type: String},
-  website: {type: String},
+  email: {type: String, lowercase: true},
+  website: {type: String, lowercase: true},
   locations: [locationSchema],
   images: [imageSchema],
   tags: {type: [String], lowercase: true, trim: true},
